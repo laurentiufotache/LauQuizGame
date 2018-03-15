93426dce424308223365305733fdd51037f67378
Laurentiu Fotache <34236177+laurentiufotache@users.noreply.github.com>
2018-03-15 14:36:05 +0200

    final release version

--- app/src/main/java/com/example/android/myquiz/MainActivity.java
+++ app/src/main/java/com/example/android/myquiz/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.android.myquiz;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -13,23 +10,21 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    // create and initialize the integer variable score
+
+    // create and initialize the integer variable who keeps the score
     int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_main);
+        //disable popping up the on-screen keyboard without touching the EditText when we change layout from landscape to portrait
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     // This method returns the score for the first question.
     public int editTextQ1(int score) {
-        // Casting the EditText for the first question
+        // Casting(finding) the EditText for the first question in order to calculate the score
         EditText q1 = findViewById(R.id.q1EditText);
         // Gets the answer for the first question and remove the unwanted spaces
         String a1 = q1.getText().toString().trim();
@@ -43,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     // This method returns the score for the second question.
     public int checkBoxQ2(int score) {
-        //Casting the check boxes for the second question
+        // Casting(finding) all the the check boxes for the second question in order to calculate the score
         CheckBox q2r1 = findViewById(R.id.q2r1_checkbox);
         CheckBox q2r2 = findViewById(R.id.q2r2_checkbox);
         CheckBox q2r3 = findViewById(R.id.q2r3_checkbox);
@@ -57,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
     // This method returns the score for the third question.
     public int checkBoxQ3(int score) {
-        // Casting the check boxes for the third question
+        // Casting(finding) all the check boxes for the third question in order to calculate the score
         CheckBox q3r1 = findViewById(R.id.q3r1_checkbox);
         CheckBox q3r2 = findViewById(R.id.q3r2_checkbox);
         CheckBox q3r3 = findViewById(R.id.q3r3_checkbox);
@@ -71,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     // This method returns the score for the fourth question.
     public int onRadioButtonQ4(int score) {
-        //Casting the radio button for the fourth question
+        // Casting(finding) the first radio button, the right answer for the fourth question, in order to calculate the score
         RadioButton checkedQ4a1 = findViewById(R.id.radio_q4a1);
         //adds one point, only if the first radio button is selected.
         if (checkedQ4a1.isChecked()) {
@@ -82,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     // This method returns the score for the fifth question.
     public int onRadioButtonQ5(int score) {
-        //Casting the radio button for the fifth question
+        // Casting(finding) the sixth radio button, the right answer for the fifth question, in order to calculate the score
         RadioButton checkedQ5a6 = findViewById(R.id.radio_q5a6);
         //adds one point, only if the sixth radio button is selected.
         if (checkedQ5a6.isChecked()) {
@@ -93,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
 
     // This method is called when the submitScore button is clicked.
     public void submitScore(View view) {
-
         //set the score to 0
         score = 0;
         // evaluates the first question and updates the score
@@ -107,28 +101,20 @@ public class MainActivity extends AppCompatActivity {
         // evaluates the fifth question and updates the score
         score = onRadioButtonQ5(score);
         //compare the score and the user receive a toast message
-        // in case the score is 5, he won, else he should give another try
+        // in case the score is 5, the user won, else the player should give another try
         if (score == 5) {
-            Toast.makeText(this, getString(R.string.succeed) + score + "!", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addCategory(Intent.CATEGORY_HOME);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+            Toast.makeText(this, getString(R.string.succeed) + score + "! " + getString(R.string.bye), Toast.LENGTH_LONG).show();
             finish();
         } else {
             Toast.makeText(this, getString(R.string.score) + score + "! " + getString(R.string.again), Toast.LENGTH_SHORT).show();
         }
     }
 
-
     //  that method is for exit the app
-
     public void exitApp(View view) {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_HOME);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+        Toast.makeText(this, getString(R.string.quit), Toast.LENGTH_LONG).show();
         finish();
+
     }
 }
 
